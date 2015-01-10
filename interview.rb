5f4fd4a635d1506707dad7fee8609c4391506947
Elise Worthy <elise.worthy@gmail.com>
2015-01-10 10:19:54 -0800

    refactor

--- interview.rb
+++ interview.rb
@@ -2,42 +2,34 @@ class Interview
   require 'csv'
   attr_reader :company, :time_slot, :student_id, :room
 
-  def initialize(company, student_id, room, day, time)
-    @company    = company
-    @student_id = student_id
-    @room       = room
-    @time_slot  = time_slot(day, time)
+  def initialize(attrs)
+    @company    = attrs["company"]
+    @student_id = attrs["student_id"]
+    @room       = attrs["room"]
+    @time_slot  = set_time_slot(attrs["day"], attrs["time"])
   end
 
   def self.all
-    interviews = []
-    CSV.foreach("interviews.csv", headers: true) do |row|
-      int = Interview.new(
-        row["company"],
-        row["student_id"],
-        row["room"],
-        row["day"],
-        row["time"]
-      )
-      interviews << int
+    @all ||= CSV.read("interviews.csv", headers: true).map do |row|
+      Interview.new(row)
     end
-    interviews
   end
 
   def self.find_all_by(col, val)
-    array = []
-    self.all.each do |int|
-      if int.send(col).to_s.downcase == val.to_s.downcase
-        array << int
-      end
+    val = val.to_s.downcase
+    self.all.select do |interview|
+      interview if interview[col].to_s.downcase == val
     end
-    array
   end
 
-  def time_slot(day, time)
+  def [](col)
+    instance_eval(col.to_s)
+  end
+
+  def set_time_slot(day, time)
     time.insert(0, "0") if time.length == 4
     DateTime.strptime("#{day} #{time}", "%m/%d/%y %I:%M")
   end
 end
 
-Interview.parse_csv
+Interview.all
